b580fd0e7b83b8f049617e25ca833b843d706892
isma91 <ismaydogmus@gmail.com>
2016-03-04 15:12:27 +0100

    Adding an autoload

--- lib/Ismaspace/Core.php
+++ lib/Ismaspace/Core.php
@@ -11,6 +11,7 @@
  * @link     https://github.com/isma91/isma_framework
  */
 namespace Ismaspace;
+use app\controllers;
 /**
  * Class Core
  *
@@ -27,26 +28,57 @@ namespace Ismaspace;
  */
 class Core
 {
-	static public function Run ()
+	/**
+     * Register_autoload
+     *
+     * A personal autoload
+     *
+     * @return Nothing
+     */
+	public static function register_autoload()
 	{
-		if (isset($_GET["page"])) {
-			if (file_exists(constant("controllers_path") . ucfirst($_GET["page"]) . "Controller.php")) {
-				require_once(constant("controllers_path") . ucfirst($_GET["page"]) . "Controller.php");
-				$class_name = ucfirst($_GET["page"] . "Controller");
-				$class = new $class_name();
-				if (empty($_GET['action'])) {
-					$action = 'indexAction';
-				} else {
-					$action = $_GET['action'] . 'Action';
-				}
-				if (!method_exists($class, $action)) {
-					var_dump("Controller " . ucfirst($_GET["page"]) . "Controller" . " have not a method called " . $action . " !!!");
-				} else {
-					$class->$action();
-				}
-			} else {
-				var_dump("Controller " . ucfirst($_GET["page"]) . "Controller" . " not found !!!");
-			}
-		}
+		spl_autoload_register('self::auto_include_once', true);
 	}
+    /**
+     * Auto_include_once
+     *
+     * Add automatically some include of the given class name
+     *
+     * @param string; $class_name name of the called class
+     *
+     * @return Nothing
+     */
+    public static function auto_include_once ($class_name)
+    {
+    	var_dump("auto_include_once => " . $class_name);
+    	if (substr($class_name, 0, 9) === "Ismaspace") {
+    		$class = str_replace("Ismaspace\\", "", $class_name);
+    		if ($class === "Controller") {
+    			include_once constant("controllers_path") . $class . ".php";
+    		}
+    	}
+    }
+    public static function Run ()
+    {
+    	Core::register_autoload();
+    	if (isset($_GET["page"])) {
+    		if (file_exists(constant("controllers_path") . ucfirst($_GET["page"]) . "Controller.php")) {
+    			require_once(constant("controllers_path") . ucfirst($_GET["page"]) . "Controller.php");
+    			$class_name = 'app\controllers\\' . ucfirst($_GET["page"] . "Controller");
+    			$class = new $class_name();
+    			if (empty($_GET['action'])) {
+    				$action = 'indexAction';
+    			} else {
+    				$action = $_GET['action'] . 'Action';
+    			}
+    			if (!method_exists($class, $action)) {
+    				var_dump("Controller " . ucfirst($_GET["page"]) . "Controller" . " have not a method called " . $action . " !!!");
+    			} else {
+    				$class->$action();
+    			}
+    		} else {
+    			var_dump("Controller " . ucfirst($_GET["page"]) . "Controller" . " not found !!!");
+    		}
+    	}
+    }
 }
