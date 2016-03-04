f3239730a834594ed2919c4ef4dfbd98d567befb
isma91 <ismaydogmus@gmail.com>
2016-03-04 16:45:46 +0100

    adding an exemple to understand the model

---
+++ app/models/TestTable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* isma_framework
+* 
+* PHP Version 5.6.17
+*
+* @category FrameWork
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://www.gnu.org/licenses/agpl-3.0.fr.html
+* @link     https://github.com/isma91/isma_framework
+*/
+namespace app\models;
+use \Ismaspace\Model;
+/**
+ * Class Test
+ *
+ * A test model to understand the framework
+ * 
+ * PHP Version 5.6.17
+ *
+ * @category Model
+ * @author   isma91 <ismaydogmus@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.fr.html
+ * @link     https://github.com/isma91/isma_framework/blob/master/app/models/TestModel.php
+ */
+class TestTable extends Model
+{
+}
