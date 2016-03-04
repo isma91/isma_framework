9b678e6cd22952e6eccc01be0a49c4ba74647cbf
isma91 <ismaydogmus@gmail.com>
2016-03-04 11:52:53 +0100

    adding IndexController for exemple

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* isma_framework
+* 
+* PHP Version 5.6.17
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://www.gnu.org/licenses/agpl-3.0.fr.html
+* @link     https://github.com/isma91/isma_framework
+*/
+/**
+ * Class Index
+ *
+ * Some Class 
+ * 
+ * PHP Version 5.6.17
+ *
+ * @category Controller
+ * @package  Controller
+ * @author   isma91 <ismaydogmus@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.fr.html
+ * @link     https://github.com/isma91/isma_framework/blob/master/app/controllers/IndexController.php
+ */
+
+class IndexController
+{
+	
+	function __construct()
+	{
+		var_dump("Hello i'm the class " . __CLASS__);
+	}
+}
