67355ccfa8aa10048fd640f3f6cb8c77d323697c
isma91 <ismaydogmus@gmail.com>
2016-03-04 12:35:15 +0100

    changing the array to multiple constant

--- config.php
+++ config.php
@@ -23,12 +23,18 @@
  * @license  http://www.gnu.org/licenses/agpl-3.0.fr.html
  * @link     https://github.com/isma91/isma_framework/blob/master/config.php
  */
-/* Array to have some path easier  */
-$path_array = array(
-	"framework" => rtrim(__DIR__, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR,
-	"lib" => "lib" . DIRECTORY_SEPARATOR,
-	"public" => "public" . DIRECTORY_SEPARATOR,
-	"controllers" => "app" . DIRECTORY_SEPARATOR . "controllers" . DIRECTORY_SEPARATOR,
-	"models" => "app" . DIRECTORY_SEPARATOR . "models" . DIRECTORY_SEPARATOR,
-	"views" => "app" . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR
-	);
+/* DO NO CHANGES THESE CONSTANT !!
+ *
+ * We define an alias for DIRECTORY_SEPARATOR
+ * And we define some important path like the controllers, models etc...
+ * These constant are created for be used more quickly than the original path
+ */
+if (!defined("DS")) {
+	define('DS', DIRECTORY_SEPARATOR);
+}
+define("framework_path", rtrim(__DIR__, constant("DS")) . constant("DS"));
+define("lib_path", constant("framework_path") . "lib" . constant("DS"));
+define("public_path", constant("framework_path") . "public" . constant("DS"));
+define("controllers_path", constant("framework_path") . "app" . constant("DS") . "controllers" . constant("DS"));
+define("models_path", constant("framework_path") . "app" . constant("DS") . "models" . constant("DS"));
+define("views_path", constant("framework_path") . "app" . constant("DS") . "views" . constant("DS"));
