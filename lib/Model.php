44e22645a6bafed3b985f64737bf03de046db877
isma91 <ismaydogmus@gmail.com>
2016-03-07 15:29:19 +0100

    adding the create_field

--- lib/Model.php
+++ lib/Model.php
@@ -80,7 +80,7 @@ abstract Class Model
      *
      * @return Array
      */
-    public function find_one($where = null, $placeholders=array())
+    public function find_one ($where = null, $placeholders=array())
     {
     	$className = get_called_class();
     	if (preg_match('/app/', $className)) {
@@ -105,4 +105,139 @@ abstract Class Model
     		return $value;
     	}
     }
+	/*
+	 * Create_field
+	 *
+	 * Create a filed to add in the table with Create_table
+	 *
+	 * @param string; $type    Type of the field
+	 * @param string; $name    Name of the field
+	 * @param string: $length  Length of the value of the field
+	 * @param string: $options Option for the field
+	 * @param string; $default If the field have a default value
+	 *
+	 * @return string; The sql query
+	 */
+	public function create_field($type, $name, $length = null, $options = null, $default = null)
+	{
+		if (!empty($type) && !empty($name)) {
+			if ($options === null) {
+				$options = "";
+			}
+			if ($default === null) {
+				$default = "";
+			}
+			if ($length === null) {
+				$length = "";
+			}
+			if ($default === null) {
+				$default = "";
+			} else {
+				$default = "DEFAULT" . " '" . $default . "'";
+			}
+			$type_name = strtoupper($type);
+			switch ($type) {
+				case "blob":
+					$length = "";
+					$default = "";
+					break;
+				case "boolean":
+					$length = "";
+					$default = "";
+					break;
+				case "char":
+					break;
+				case "date":
+					$length = "";
+					$default = "";
+					break;
+				case "date_time":
+					$length = "";
+					$default = "";
+					break;
+				case "decimal":
+					break;
+				case "double":
+					$length = "";
+					break;
+				case "enum":
+					if ($length === null) {
+						return;
+					}
+					if ($default !== null) {
+						$validate_default = false;
+						foreach (explode(",", $length) as $value) {
+							if ($default === $value) {
+								$validate_default = true;
+								break;
+							}
+						}
+						if ($validate_default === true) {
+							$default = "DEFAULT '" . " " . $default . "'";
+						} else {
+							$default = "DEFAULT NULL";
+						}
+					}
+					break;
+				case "float":
+					break;
+				case "increments":
+					$options = "NOT NULL AUTO_INCREMENT";
+					$default = "";
+					break;
+				case "integer":
+					break;
+				case "longtext":
+					$default = "";
+					$length = "";
+					break;
+				case "mediumint":
+					break;
+				case "mediumtext":
+					$default = "";
+					$length = "";
+					break;
+				case "smallint":
+					break;
+				case "varchar":
+					break;
+				case "text":
+					$length = "";
+					break;
+				case "time":
+					$length = "";
+					$default = "";
+					break;
+				case "timestamp":
+					$length = "";
+					$default = "DEFAULT CURRENT_TIMESTAMP";
+					break;
+				default:
+					break;
+			}
+			return "$name" . " " . "$type_name" . "$length" . " " . $options . " " . $default;
+		}
+		return "";
+	}
+	/*
+	 * Create_table
+	 *
+	 * Create a table in the database of the framework
+	 *
+	 * @param string; $table_name  the table name
+	 * @param array;  $array_field array of the field and type
+	 *
+	 * @return void
+	 */
+	/*public function create_table ($table_name, array $array_field)
+	{
+		if (!empty($table_name) && !empty($array_field)) {
+			$db = self::$pdo;
+			if ($db === null) {
+				var_dump("You must check your database configuration !!");
+			} else {
+				$sql = "CREATE TABLE IF NOT EXISTS `$table_name(";
+			}
+		}
+	}*/
 }
